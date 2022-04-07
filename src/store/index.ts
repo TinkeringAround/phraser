d9204ceb79a4675e576daf2b1bb4c5b14e7964c6
TinkeringAround <M1j2C3r4O5s6S7Github!!>
2022-04-07 13:49:17 +0200

    add store

---
+++ src/store/index.ts
@@ -0,0 +1,81 @@
+import create, { SetState, State } from "zustand";
+import { TSnippet, TSong } from "../libs/util";
+import {
+  addErrorRecipe,
+  createSnippetRecipe,
+  createSongRecipe,
+  deleteSnippetRecipe,
+  deleteSongRecipe,
+  setDictionaryRecipe,
+  updateSongRecipe,
+} from "./recipes";
+import { IDictionary, Language } from "../libs/dictionary";
+
+export interface PhraserStateDictionary {
+  [Language.german]: string[];
+  [Language.english]: string[];
+  [Language.french]: string[];
+}
+
+export interface PhraserState extends State {
+  errors: string[];
+  addError: (error: string) => void;
+
+  song: TSong | null;
+  setSong: (song: TSong | null) => void;
+
+  songs: TSong[];
+  createSong: (song: TSong) => void;
+  deleteSong: (id: string) => void;
+  setSongs: (songs: TSong[]) => void;
+  updateSong: (song: TSong) => void;
+
+  snippets: TSnippet[];
+  createSnippet: (snippet: TSnippet) => void;
+  deleteSnippet: (id: string) => void;
+  setSnippets: (snippets: TSnippet[]) => void;
+
+  isProcessing: boolean;
+  setIsProcessing: (isProcessing: boolean) => void;
+
+  dictionary: PhraserStateDictionary;
+  setDictionary: (
+    germanDict: IDictionary,
+    englishDict: IDictionary,
+    frenchDict: IDictionary
+  ) => void;
+}
+
+export const usePhraser = create<PhraserState>(
+  (set: SetState<PhraserState>) => ({
+    errors: [],
+    addError: (error: string) => set((state) => addErrorRecipe(error, state)),
+
+    song: null,
+    setSong: (song: TSong | null) => set({ song }),
+
+    songs: [],
+    createSong: (song) => set((state) => createSongRecipe(song, state)),
+    deleteSong: (id: string) => set((state) => deleteSongRecipe(id, state)),
+    setSongs: (songs) => set({ songs }),
+    updateSong: (song) => set((state) => updateSongRecipe(song, state)),
+
+    snippets: [],
+    createSnippet: (snippet: TSnippet) =>
+      set((state) => createSnippetRecipe(snippet, state)),
+    deleteSnippet: (id: string) =>
+      set((state) => deleteSnippetRecipe(id, state)),
+    setSnippets: (snippets) => set({ snippets }),
+
+    isProcessing: false,
+    setIsProcessing: (isProcessing) => set({ isProcessing }),
+
+    dictionary: {
+      german: [],
+      english: [],
+      french: [],
+    },
+    setDictionary: (ge: IDictionary, en: IDictionary, fr: IDictionary) =>
+      set({ dictionary: setDictionaryRecipe(ge, en, fr) }),
+  })
+);
---
+++ src/store/recipes.ts
@@ -0,0 +1,46 @@
+import { PhraserState, PhraserStateDictionary } from "./index";
+import { TSnippet, TSong } from "../libs/util";
+import { IDictionary, Language } from "../libs/dictionary";
+
+export const addErrorRecipe = (error: string, state: PhraserState) => ({
+  errors: [...state.errors, error],
+});
+
+export const createSongRecipe = (song: TSong, state: PhraserState) => ({
+  song: song,
+  songs: [...state.songs, song],
+});
+
+export const deleteSongRecipe = (id: string, state: PhraserState) => ({
+  song: state.song?.id === id ? null : state.song,
+  songs: state.songs.filter((song) => song.id !== id),
+});
+
+export const deleteSnippetRecipe = (id: string, state: PhraserState) => ({
+  snippets: state.snippets.filter((snippet) => snippet.id !== id),
+});
+
+export const updateSongRecipe = (song: TSong, state: PhraserState) => {
+  const songs = [...state.songs];
+  const songIndex = songs.findIndex((s) => s.id === song.id);
+  songs[songIndex] = song;
+
+  return { songs, song };
+};
+
+export const createSnippetRecipe = (
+  snippet: TSnippet,
+  state: PhraserState
+) => ({
+  snippets: [...state.snippets, snippet],
+});
+
+export const setDictionaryRecipe = (
+  ge: IDictionary,
+  en: IDictionary,
+  fr: IDictionary
+): PhraserStateDictionary => ({
+  [Language.german]: ge.words,
+  [Language.english]: en.words,
+  [Language.french]: fr.words,
+});
